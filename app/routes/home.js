ed6be188fbb431807fbe6548252fabcc9f7d9518
Rodrigo Marzullo <rodrigo.marzullo@gmail.com>
2018-02-14 16:11:16 -0200

    mud home

--- app/routes/home.js
+++ app/routes/home.js
@@ -4,33 +4,6 @@
 module.exports = function(app){
 
 
-
-
-
-	app.get("/produtos",function(req, res) {
-
-	        var connection = app.infra.connectionFactory();
-	        var ProdutosDAO = new app.infra.ProdutosDAO(connection);
-
-	        ProdutosDAO.lista(function(err, results){
-
-	              res.format({
-	                html: function(){
-	                    res.render("lista",{lista:results});
-	                },
-	                json: function(){
-	                    res.json(results);
-	                }
-	            });
-
-	        });
-
-	        connection.end();
-
-	    });
-
-
-
 			app.get("/produtos",function(req, res) {
 
 							var connection = app.infra.connectionFactory();
